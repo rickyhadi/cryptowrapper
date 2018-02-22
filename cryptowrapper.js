66ac67ace2726c813e97ec21e470e21f2a100259
Ricky Hadi <ricky.hadi@mvig.net>
2018-02-22 16:04:40 +0700

    init

---
+++ cryptowrapper.js
@@ -0,0 +1,25 @@
+var crypto = require('crypto');
+
+function cryptowrapper(appConfig){
+	var key = appConfig.App.Crypto.Key;
+	var iv = appConfig.App.Crypto.IV;
+	var cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+	var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+	
+    var obj = {};
+    obj.encrypt = function(input){
+        var encrypted = cipher.update(input, 'utf8', 'binary');
+        encrypted += cipher.final('binary');
+        hexVal = new Buffer(encrypted, 'binary');
+        newEncrypted = hexVal.toString('hex');
+        return newEncrypted;
+    };
+    obj.decrypt = function(input){
+        var decrypted = decipher.update(input, 'hex', 'binary');
+        decrypted += decipher.final('binary');
+        return decrypted;
+    };
+    return obj;
+}
+
+module.exports = cryptowrapper;
